dabb5410e763c372dfb7c05306c2a44c1c35b597
finwo <finwo@pm.me>
2023-07-08 22:22:19 +0200

    Fix building

--- src/http-parser.h
+++ src/http-parser.h
@@ -53,8 +53,8 @@ struct http_parser_pair {
 
 // Header management
 char *http_parser_header_get(struct http_parser_message *subject, const char *key);
-void http_parser_header_set(struct http_parser_message *subject ,const char *key, char *value);
-void http_parser_header_add(struct http_parser_message *subject ,const char *key, char *value);
+void http_parser_header_set(struct http_parser_message *subject ,const char *key, const char *value);
+void http_parser_header_add(struct http_parser_message *subject ,const char *key, const char *value);
 void http_parser_header_del(struct http_parser_message *subject, const char *key);
 
 struct http_parser_pair    * http_parser_pair_init(void *udata);
