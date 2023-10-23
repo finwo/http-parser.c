6201d0cf113c4af64c6ce99dde1861c511e350ca
finwo <finwo@pm.me>
2023-10-24 00:41:08 +0200

    Using finwo/asprintf library instead of maintaining apsrintf polyfill here

--- src/http-parser.c
+++ src/http-parser.c
@@ -8,6 +8,7 @@ extern "C" {
 #include <string.h>
 #include <strings.h>
 
+#include "finwo/asprintf.h"
 #include "finwo/strnstr.h"
 #include "tidwall/buf.h"
 
@@ -48,19 +49,6 @@ int xtoi(char *str) {
 
   return sign * i;
 }
-
-#ifndef _GNU_SOURCE
-// Polyfill asprintf, comes from stdio when _GNU_SOURCE is defined
-char * asprintf(char **restrict strp, const char *restrict fmt, ...) {
-  va_list argptr;
-  va_start(argptr, fmt);
-  int size = vsnprintf(NULL, 0, fmt, argptr);
-  char *output = calloc(1, size + 1);
-  vsnprintf(output, size + 1, fmt, argptr);
-  va_end(argptr);
-  return output;
-}
-#endif
 
 /**
  * Frees everything in a header that was malloc'd by http-parser
